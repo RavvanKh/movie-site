a084276138dc5c973836b7497b9f549a86c8acb4
RavanKhaligov <112714179+RavanKhaligov@users.noreply.github.com>
2023-07-15 22:54:44 +0400

    Update header.jsx

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -6,14 +6,14 @@ const Header = () => {
   return (
     <header className={`${styles.header} container fluid` }>
         <div className={styles.headerWrapper}>
-            <Link href='' className={styles.logo}>
+            <Link href='/' className={styles.logo}>
                 <FaPlayCircle/>
                 NETFILMS
             </Link>
             <nav className={styles.navigationMenu}>
-                <Link href=''>Movies</Link>
-                <Link href=''>Series</Link>
-                <Link href=''>Kids</Link>
+                <Link href='/'>Movies</Link>
+                <Link href='/'>Series</Link>
+                <Link href='/'>Kids</Link>
             </nav>
         </div>
     </header>
